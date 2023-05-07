ad62d4a4337a7fe90bcdcb3e81c2e5257ffb163d
Aspkar <saeedtavakoli001@gmail.com>
2023-05-07 17:55:54 +0430

    adding location table

---
+++ Entities/Forecast/Location.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Forecast
+{
+    public class Location : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+    }
+
+    public class LocationConfiguration : IEntityTypeConfiguration<Location>
+    {
+        public void Configure(EntityTypeBuilder<Location> builder)
+        {
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(200);
+        }
+    }
+
+}
