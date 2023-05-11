e44da79066005fbb9ad2bd8bca7d98a4a2520b07
Aspkar <saeedtavakoli001@gmail.com>
2023-05-11 18:24:00 +0330

    Cleaning Startup Class by adding configuration to framework layer and adding AutoMapper and Swagger configs

--- WeatherForecast/Startup.cs
+++ WeatherForecast/Startup.cs
@@ -1,62 +1,67 @@
+using Common;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using WebFramework.Configuration;
+using WebFramework.CustomMapping;
 using WebFramework.Middlewares;
+using WebFramework.Swagger;
 
 namespace WeatherForecast
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        private readonly SiteSettings _siteSetting;
+        private readonly IWebHostEnvironment _currentEnvironment;
+
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-        }
 
-        public IConfiguration Configuration { get; }
+            _siteSetting = configuration.GetSection(nameof(SiteSettings)).Get<SiteSettings>();
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<SiteSettings>(Configuration.GetSection(nameof(SiteSettings)));
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WeatherForecast", Version = "v1" });
-            });
+            services.AddDbContext(Configuration);
+
+            services.AddMinimalMvc();
+
+            services.AddSwagger();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             app.UseCustomExceptionHandler();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WeatherForecast v1"));
-            }
+            app.UseHsts(env);
 
             app.UseHttpsRedirection();
 
+            app.UseSwaggerAndUI();
+
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
+            //Use this config just in Develoment (not in Production)
+            app.UseCors(config => config.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+            app.UseEndpoints(config =>
             {
-                endpoints.MapControllers();
+                config.MapControllers();
             });
+
         }
     }
 }
---
+++ WebFramework/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Common;
+using Common.Exceptions;
+using Common.Utilities;
+using Data;
+using Data.Repositories;
+using ElmahCore.Mvc;
+using ElmahCore.Sql;
+using Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebFramework.Configuration
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options
+                    .UseSqlServer(configuration.GetConnectionString("SqlServer"));
+            });
+        }
+
+        public static void AddMinimalMvc(this IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson(option =>
+            {
+                option.SerializerSettings.Converters.Add(new StringEnumConverter());
+                option.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            });
+            services.AddSwaggerGenNewtonsoftSupport();
+        }
+    }
+}
