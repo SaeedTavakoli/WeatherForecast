257f7e280a422395cf9e4489cedb6ec13290be59
Aspkar <saeedtavakoli001@gmail.com>
2023-05-28 08:22:15 +0330

    First Tests

--- WeatherForecast/Controllers/v1/LocationsController.cs
+++ WeatherForecast/Controllers/v1/LocationsController.cs
@@ -7,6 +7,9 @@ using WebFramework.Api;
 
 namespace WeatherForecast.Controllers.v1
 {
+    /// <summary>
+    /// Location Crud Controller
+    /// </summary>
     public class LocationsController : CrudController<LocationDto, LocationSelectDto, Location, int>
     {
         public LocationsController(IRepository<Location> repository, IMapper mapper)
