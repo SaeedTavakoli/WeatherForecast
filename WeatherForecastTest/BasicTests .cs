a37f412e314911dfc431ded8cb01e39271a80266
Saeed Tavakoli <saeedtavakolii@hotmail.com>
2023-06-03 16:01:29 +0430

    Basic Integerated Test

---
+++ WeatherForecastTest/BasicTests .cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherForecast;
+using Xunit;
+
+namespace WeatherForecastTest
+{
+    public class BasicTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        public BasicTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/api/v1/Locations/Get");
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+        }
+    }
+}
